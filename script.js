d84d4ef7d8769ae39f572f8f980126c51c7ade39
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-10-21 18:03:23 +0200

    second part

--- script.js
+++ script.js
@@ -34,12 +34,10 @@ let team = [
 
 const teamContainer = document.querySelector(".team-container")
 
-
 // Ciclo per ogni persona del team
 for (let i = 0; i < team.length; i++) {
-  let person = team[i];
-
-  console.log(person); // Persone del team
+  const person = team[i];
+  console.log("Persone del team:",person); // Persone del team
 
   teamContainer.innerHTML += `<div class="team-card">
                                 <div class="card-image">
@@ -56,3 +54,26 @@ for (let i = 0; i < team.length; i++) {
 
 
 
+const newName = document.getElementById("name");
+const newRole = document.getElementById("role");
+const newImg = document.getElementById("image");
+
+const btnAddMember = document.getElementById("addMemberButton");
+
+// Aggiungere nuova persona
+btnAddMember.addEventListener("click", function () {
+
+  teamContainer.innerHTML += `<div class="team-card">
+                                <div class="card-image">
+                                  <img src="img/${newImg.value}" alt="${newName.value}"/>
+                                </div>
+                                <div class="card-text">
+                                  <h3>${newName.value}</h3>
+                                  <p>${newRole.value}</p>
+                                </div>
+                              </div> `;
+
+newImg.value = "";                                        
+newName.value = "";          
+newRole.value = "";          
+});
